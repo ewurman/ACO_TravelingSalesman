661aa807f39f07c03e63346375a032ca80f108fc
Ian Squiers <iansquiers321@gmail.com>
2018-11-06 20:08:20 -0500

    ACS almost ready

--- ACS.hpp
+++ ACS.hpp
@@ -35,15 +35,19 @@ using namespace std;
 
 class ACS : ACO {
     private:
-
+        vector<int> btsf;
+        double best_eval;
+    
         double epsilon; //wear away factor
         double tau_naught; // Minimum pheremone concentration
         double q_naught; // probability of choosing greedily next leg
-        void run_tour(char** dist, char** pheromones);
+        void run_tour(double** dist, double** pheromones);
         int select_next(int curr_id);
-        int greedy_selection(int curr_city, vector<int> cities_remaining, char** dist, char** pheromones);
-        int prob_selection(int curr_city, vector<int> cities_remaining, char** dist, char** pheromones);
-    double sum_options(int curr_city, vector<int> cities_remaining, char** dist, char** pheromones);
+        int greedy_selection(int curr_city, vector<int> cities_remaining, double** dist, double** pheromones);
+        int prob_selection(int curr_city, vector<int> cities_remaining, double** dist, double** pheromones);
+        double sum_options(int curr_city, vector<int> cities_remaining, double** dist, double** pheromones);
+        void local_pupdate(int i, int j);
+        void global_pupdate(vector<int> best_tour);
     public:
 };
 
