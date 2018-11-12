1f93c23d257e1f6cbcc7e40e644d707baa32390e
Ian Squiers <iansquiers321@gmail.com>
2018-11-12 16:55:01 -0500

    plotter

--- outputReformater.py
+++ outputReformater.py
@@ -1,35 +1,98 @@
 import sys
-
+import os
+import statistics
+import matplotlib.pyplot as plt
+import numpy as np
 
 #print(sys.argv)
 
-
-
-
-
-
 def __main__():
-    inputFile = sys.argv[1];
-    with open(inputFile, 'r') as f:
-        lines = f.readlines();
-        print(lines);
+    directory = "/Users/iansquiers/Desktop/NI/ACO_TravelingSalesman/OutputFiles"
+    print(os.listdir(directory))
 
-    benchmarks = [x.split('x')[0] for x in lines[0].split(',')[:-2]];
-    print (benchmarks)
-    lines = [x for x in lines[1:] if x[0] != "#"];
-    print(lines);
+    allfiles = []
+    for thing in os.listdir(directory):
+        if thing.endswith('.csv'):
+            allfiles.append(thing)
+    allfiles.sort()
+    print(allfiles)
 
-    times = [];
-    counts = [];
-    for line in lines:
-        x = line.split(":");
-        time = x[0];
-        count = x[1];
+    this = input("helo")
+    for j in range(4):
+        filename = "/Users/iansquiers/Desktop/NI/ACO_TravelingSalesman/OutputFiles/" + allfiles[j]
+        title = allfiles[j][3:-4]
+        if filename.endswith('.csv'):
+            print(filename)
+            
+            with open(filename, 'r') as f:
+                lines = f.readlines()
+                timelabel = []
+                plotcount = []
+                times = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+                counts = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+                for line in lines[3:]:
+                    line = line.split(",")
+                    print("line = ", line)
+                    
+                    for i in range(len(line)-2):
+                        cell = line[i]
+                        x = cell.split(":")
+                        time = float(x[0])
+                        count = float(x[1])
+                        times[i].append(time)
+                        counts[i].append(count)
+            
+                print("counts = ", counts)
+                for i in range(len(line)-2):
+                    plotcount.append(statistics.mean(counts[i]))
+                    timelabel.append(max(times[i]))
+                
+                print("plotcount = ", plotcount)
+                print(timelabel)
+                this = input("hello")
+                ind = np.arange(len(plotcount))
+                width = 0.35
+            if j % 2 == 0 :
+                fig, ax = plt.subplots()
+                print(ind)
+                print(counts)
+                rects1 = ax.bar(ind, plotcount, width, color='r')
+                for i in range(len(rects1)):
+                    height = rects1[i].get_height()
+                    ax.text(rects1[i].get_x() + rects1[i].get_width()/2., 1.05*height,
+                            '%d' % timelabel[i],
+                            ha='center', va='bottom')
+            else:
+                rects2 = ax.bar(ind+width, plotcount, width, color='b')
+                
+                for i in range(len(rects2)):
+                    height = rects2[i].get_height()
+                    ax.text(rects2[i].get_x() + rects2[i].get_width()/2., 1.05*height,
+                            '%d' % timelabel[i],
+                            ha='center', va='bottom')
+                
+                
+                ax.set_ylabel('Scores')
+                ax.set_title('Distribution of Tour Termination, and Time Taken to Achieve Optimality of ' + title + ' Cities')
+                ax.set_xticks(ind + width / 2)
+                ax.set_xticklabels(('1.4' ,'1.35' ,'1.325' ,'1.30' ,'1.275' ,'1.25' ,'1.225' ,'1.20' ,'1.175' ,'1.15' ,'1.125' ,'1.10' ,'1.075' ,'1.05' ,'1.025', '1.0' ))
+                ax.legend((rects1[0], rects2[0]), ('ACS','EAS'))
+                plt.show()
+            i+=1
+        
+            
 
 
 
+                
 
 
+
+    
+    
+    
+    
+    
     print("Ending the file")
 
 __main__()
