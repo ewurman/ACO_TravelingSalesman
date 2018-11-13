18eb24818f11475b88b32c296990131f3177b72d
Ian Squiers <iansquiers321@gmail.com>
2018-11-12 23:23:09 -0500

    graphs

--- outputReformater.py
+++ outputReformater.py
@@ -17,10 +17,9 @@ def __main__():
     allfiles.sort()
     print(allfiles)
 
-    this = input("helo")
-    for j in range(4):
+    for j in range(8):
         filename = "/Users/iansquiers/Desktop/NI/ACO_TravelingSalesman/OutputFiles/" + allfiles[j]
-        title = allfiles[j][3:-4]
+        title = allfiles[j][:4]
         if filename.endswith('.csv'):
             print(filename)
             
@@ -49,7 +48,7 @@ def __main__():
                 
                 print("plotcount = ", plotcount)
                 print(timelabel)
-                this = input("hello")
+                # this = input("hello")
                 ind = np.arange(len(plotcount))
                 width = 0.35
             if j % 2 == 0 :
@@ -59,7 +58,8 @@ def __main__():
                 rects1 = ax.bar(ind, plotcount, width, color='r')
                 for i in range(len(rects1)):
                     height = rects1[i].get_height()
-                    ax.text(rects1[i].get_x() + rects1[i].get_width()/2., 1.05*height,
+                    if timelabel[i] != 0 and timelabel[i] != -1:
+                        ax.text(rects1[i].get_x() + rects1[i].get_width()/2., 1.001*height,
                             '%d' % timelabel[i],
                             ha='center', va='bottom')
             else:
@@ -67,32 +67,24 @@ def __main__():
                 
                 for i in range(len(rects2)):
                     height = rects2[i].get_height()
-                    ax.text(rects2[i].get_x() + rects2[i].get_width()/2., 1.05*height,
+                    if timelabel[i] != 0 and timelabel[i] != -1:
+                        ax.text(rects2[i].get_x() + rects2[i].get_width()/2., 1.001*height,
                             '%d' % timelabel[i],
                             ha='center', va='bottom')
                 
                 
-                ax.set_ylabel('Scores')
+                ax.set_ylabel('Count of Tours Terminating at Optimality')
                 ax.set_title('Distribution of Tour Termination, and Time Taken to Achieve Optimality of ' + title + ' Cities')
                 ax.set_xticks(ind + width / 2)
                 ax.set_xticklabels(('1.4' ,'1.35' ,'1.325' ,'1.30' ,'1.275' ,'1.25' ,'1.225' ,'1.20' ,'1.175' ,'1.15' ,'1.125' ,'1.10' ,'1.075' ,'1.05' ,'1.025', '1.0' ))
                 ax.legend((rects1[0], rects2[0]), ('ACS','EAS'))
+
+#                t = ("This is a really long string that I'd rather have wrapped so that it doesn't go outside of the figure, but if it's long enough it will go off the top or bottom!")
+#                plt.text(0.5, -.25, t, horizontalalignment='center', verticalalignment='center', transform=ax.transAxes, wrap=True)
                 plt.show()
-            i+=1
-        
-            
-
-
-
-                
-
-
-
-    
-    
-    
-    
-    
+                this = input("jhbjn")
+            j+=1
+    this = input("jhbjn")
     print("Ending the file")
 
 __main__()
